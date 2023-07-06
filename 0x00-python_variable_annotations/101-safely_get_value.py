a9212ff51d8c13dce7ca66a25438b466e190ac69
root <anthony.n.ndegwa@gmail.com>
2023-07-06 07:37:48 -0700

    More involved type annotations

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Given parameters and return values, add type annotations to function
+
+Hint: look into TypeVar
+
+def safely_get_value(dct, key, default = None):
+    if key in dct:
+        return dct[key]
+    else:
+        return default
+"""
+from typing import TypeVar, Dict, Optional, Mapping, Any, Union
+
+V = TypeVar('V')
+
+
+def safely_get_value(dct: Mapping[str, V], key: Any, default:
+                     Optional[Union[V, None]] = None) -> Union[V, Any]:
+    """More involved type annotations"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
