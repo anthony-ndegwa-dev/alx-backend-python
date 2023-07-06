77e2816a1df6b1e3ffa003bc478ad692369112bc
root <anthony.n.ndegwa@gmail.com>
2023-07-06 08:23:37 -0700

    Fix error: Argument 1 to "zoom_array" has incompatible type

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -16,14 +16,14 @@ zoom_2x = zoom_array(array)
 zoom_3x = zoom_array(array, 3.0)
 """
 
-from typing import List, Tuple
+from typing import List, Tuple, Any
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> List:
+def zoom_array(lst: List, factor: int = 2) -> List:
     """ Create copies of items in a tuple"""
     zoomed_in: List = [
         item for item in lst
-        for i in range(int(factor))
+        for i in range(factor)
     ]
     return zoomed_in
 
@@ -32,4 +32,4 @@ array = [12, 72, 91]
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, 3)
