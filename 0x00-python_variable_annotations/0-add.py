f7df5736fe14cfddc8454d548a0e653c6a2da848
root <anthony.n.ndegwa@gmail.com>
2023-07-06 04:57:52 -0700

    Function that add floats

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A function that takes float a and b as arguments and returns their
+  sum as a float. """
+
+
+def add(a: float, b: float) -> float:
+    # Sum of a + b
+    return a + b
