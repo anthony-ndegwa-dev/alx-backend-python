923462c38f3169a04ea9bfa388117e36e1503cab
root <anthony.n.ndegwa@gmail.com>
2023-07-10 23:32:29 -0700

    3:71: W291 trailing whitespace

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Asynchronous coroutine takes in an integer argument
-(max_delay, with default value of 10) named wait_random that waits for 
+(max_delay, with default value of 10) named wait_random that waits for
 random delay between 0 and max_delay (included and float value)
 seconds and eventually returns it.
 
